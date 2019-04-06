87ef4a557689c302e373233e67d7a2d0fd24c67f
YerbolIrgaliyev <yerbol.irgaliyev@gmail.com>
2019-04-07 00:58:43 +0600

    first commit

---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_29_01
+{
+    public struct Employee
+    {
+        public string Name { get; set; }
+        public Vacancies Vacancy { get; set; }
+        public int Salary { get; set; }
+        public DateTime DateWork { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name          : {Name}");
+            Console.WriteLine($"Vacancy       : {Vacancy.ToString()}");
+            Console.WriteLine($"Salary        : {Salary}");
+            Console.WriteLine($"Working since : {DateWork}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_29_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee manager = new Employee();
+            manager.Name = "Williams";
+            manager.Salary = 255555;
+            manager.Vacancy = Vacancies.Manager;
+            manager.DateWork = DateTime.Now;
+
+            manager.Print();
+        }
+    }
+}
